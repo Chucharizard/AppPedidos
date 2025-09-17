d9befbe5f07363328e0eed14b8937196a6930988
Chucho <willyguevara048@gmail.com>
2025-09-16 20:53:50 -0400

    update modelsss jefeee

--- pedidosApp/Models/OrderItem.cs
+++ pedidosApp/Models/OrderItem.cs
@@ -2,5 +2,10 @@
 {
     public class OrderItem
     {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public decimal Subtotal { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
     }
 }
--- pedidosApp/Models/OrderModel.cs
+++ pedidosApp/Models/OrderModel.cs
@@ -2,5 +2,10 @@
 {
     public class OrderModel
     {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Status { get; set; }
+        public decimal Total { get; set; }
+        public int UserId { get; set; }
     }
 }
