fb106fb58e8e39cd703bf457d5a3bbfbddd8581c
Chucho <willyguevara048@gmail.com>
2025-09-23 20:11:34 -0400

    falto una validacion xd

--- pedidosApp/Models/OrderModel.cs
+++ pedidosApp/Models/OrderModel.cs
@@ -1,11 +1,24 @@
-﻿namespace pedidosApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace pedidosApp.Models
 {
     public class OrderModel
     {
         public int Id { get; set; }
-        public DateTime OrderDate { get; set; }
-        public string Status { get; set; }
+
+        [Required(ErrorMessage = "La fecha del pedido es obligatoria")]
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "El estado es obligatorio")]
+        [StringLength(50)]
+        public string Status { get; set; } = "Pendiente";
+
+        [Range(0, double.MaxValue, ErrorMessage = "El total debe ser 0 o mayor")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Total { get; set; }
+
+        [Required(ErrorMessage = "El usuario es obligatorio")]
         public int UserId { get; set; }
     }
 }
